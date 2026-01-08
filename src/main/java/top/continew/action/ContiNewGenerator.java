58fcfebcadb88f9aee3ee6aa11569e490ce88f23
lww <lerder@foxmail.com>
2026-01-08 18:17:16 +0800

    refactor: 重构生成器单例模式为局部变量

--- src/main/java/top/continew/action/ContiNewGenerator.java
+++ src/main/java/top/continew/action/ContiNewGenerator.java
@@ -2,19 +2,13 @@ package top.continew.action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.Project;
 import top.continew.ui.MainGenerator;
 
 public class ContiNewGenerator extends AnAction {
 
-	private static MainGenerator instance = null;
-
 	@Override
 	public void actionPerformed(AnActionEvent e) {
-		Project project = e.getProject();
-		if (instance == null) {
-			instance = new MainGenerator(project);
-		}
+		MainGenerator instance = new MainGenerator(e.getProject());
 		instance.show();
 	}
 }
